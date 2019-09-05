77b05a6e21ca7b20a1a02c0f8bd50b3555d7381a
ibrahem <>
2019-09-05 13:57:52 -0700

    create register service with passport and handle the response with service own function registerResponse

--- app/Modules/UserModule/Services/AuthService.php
+++ app/Modules/UserModule/Services/AuthService.php
@@ -2,10 +2,11 @@
 
 namespace UserModule\Services;
 
-use UserModule\Http\Resources\AuthResource;
+use UserModule\Http\Resources\AuthLoginResource;
 use UserModule\Repositories\AuthRepositoryInterface; 
 use GuzzleHttp\Exception\BadResponseException;
 use GuzzleHttp\Client;
+use UserModule\Http\Resources\AuthRegisterResource;
 
 use function GuzzleHttp\json_decode;
 
@@ -18,19 +19,36 @@ class AuthService
         $this->authRepository = $authRepository;
     }  
 
-    public function login($data){ 
-         
-        $client = new Client();  
-         
+    public function register($request) 
+    {  
+        $user = $this->authRepository->create($request);
+        $access_token = $user->createToken('access_token')->accessToken; 
+        return $this->registerResponse($access_token);
+        
+    }
+    public function registerResponse($access_token) 
+    {
+        return response()->json([
+                    'data' => [
+                        'access_token' => $access_token
+                    ],
+                    'status' => 'Success', 
+                    'statusCode' => 201
+                ]);
+    }
+    public function login($data)
+    {  
+        $client = new Client();   
         try{ 
             $response = $this->oauthLoginRequest($client, $data);  
-            return new AuthResource(json_decode($response)); 
+            return new AuthLoginResource(json_decode($response)); 
          } catch(BadResponseException $e ){ 
             $this->handleBadResponseException($e); 
         }
      }
 
-    public function oauthFormParams($data){
+    public function oauthFormParams($data)
+    {
         return  [
             'grant_type' => 'password',
             'client_id'  => 2,
@@ -39,14 +57,16 @@ class AuthService
             'password' => $data->password
         ];  
     }
-    public function oauthLoginRequest($client, $data){ 
+    public function oauthLoginRequest($client, $data)
+    { 
         // Be Sure to serve different port with terminal
         $response =  $client->post('http://127.0.0.1:81/oauth/token', [
             'form_params' => $this->oauthFormParams($data)
         ]);  
         return $response->getBody();  
     } 
-    public function handleBadResponseException($e){
+    public function handleBadResponseException($e)
+    {
         if($e->getCode() == 400) { 
             $this->response_400($e); 
         } else if ($e->getCode() == 401) { 
@@ -54,16 +74,21 @@ class AuthService
         }  
         return response()->json('Something Went wrong with the serveer', $e->getCode());
     }
-    public function response_400($e){
+    public function response_400($e)
+    {
         return response()->json(
                             'Bad Request - Invalid URL',
                             $e->getCode()
                             );
     }
-    public function response_401($e){
+    public function response_401($e)
+    {
         return response()->json(
                             'invalid Request, Please enter a username or a password',
                             $e->getCode()
                             );
-    }
+    } 
+    
+  
+ 
 }
